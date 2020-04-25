efb17de92b78d3ad745d108a6211aca26e0c0843
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-04-25 12:45:40 +0530

    adding algo_1 for prime

---
+++ prime_number/algo_1.py
@@ -0,0 +1,10 @@
+'''
+
+    The solution is written in O(n) complexity. 
+    Algorithm:
+    - For a given number `n`
+      - loop over all the numbers `i` from 2 to (n-1)
+      - if `n` is divisible by `i`, it is not a prime
+    - if `n` is not divisible by any such `i`, it is a prime number
+
+'''
