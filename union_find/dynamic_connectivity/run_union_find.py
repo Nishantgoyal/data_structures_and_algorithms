de218439bd27cbba624aad0a15a5bc5174d099ce
Nishant Goyal <nishantgoyal0789@gmail.com>
2020-04-25 20:02:03 +0530

    Adding file to execute the union find class

---
+++ union_find/dynamic_connectivity/run_union_find.py
@@ -0,0 +1,25 @@
+import sys
+from union_find import UnionFind
+
+
+def main():
+    N = int(sys.argv[1])
+    uf = UnionFind(N)
+    print("Input queries in format\nU a b\nC a b\nX for exit")
+    input_string = ""
+    while(input_string != "X"):
+        input_string = input()
+        if "U " in input_string:
+            a, b = map(int, input_string.split()[1:])
+            uf.union(int(a), int(b))
+            print("Connected Components: {}".format(uf.components()))
+        if "C " in input_string:
+            a, b = map(int, input_string.split()[1:])
+            if uf.connected(int(a), int(b)):
+                print("{} and {} are connected".format(a, b))
+            else:
+                print("{} and {} are not connected".format(a, b))
+
+
+if __name__ == "__main__":
+    main()
