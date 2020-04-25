cc98a6cdecb81b7c045aab758485c7fb5b40b2c2
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-04-25 12:58:09 +0530

    parsing input from command line

--- prime_number/algo_1.py
+++ prime_number/algo_1.py
@@ -1,6 +1,11 @@
+import sys
+
 '''
 
     The solution is written in O(n) complexity. 
+
+    Input: Number `n`
+
     Algorithm:
     - For a given number `n`
       - loop over all the numbers `i` from 2 to (n-1)
@@ -8,3 +13,25 @@
     - if `n` is not divisible by any such `i`, it is a prime number
 
 '''
+
+
+def get_input():
+    '''
+        Gets the input from the command line argument. 
+        Also checks if the input is a number.
+        Throws an error if no command line argument is present
+    '''
+    if len(sys.argv) > 2:
+        raise Exception("Extra Arguments Provided...!!!")
+    elif len(sys.argv) == 1:
+        raise Exception("Arguments not provided...!!!")
+    return int(sys.argv[1])
+
+
+def main():
+    N = get_input()
+    print("Input: {}".format(N))
+
+
+if __name__ == "__main__":
+    main()
