9e35d23f489c76f594b6ac26edf7a97105ee9825
Nishant Goyal <nishantgoyal0789@gmail.com>
2020-04-27 03:39:19 +0530

    Binary Search

--- Search/run_search.py
+++ Search/run_search.py
@@ -1,7 +1,8 @@
 import time
 from random import randint
 
-from brute_search import Search
+# from brute_search import Search
+from binary_search import Search
 
 
 def main():
@@ -10,11 +11,15 @@ def main():
     last = 0
     count = 100
     search = Search()
+    rand_range = 50000
     while True:
-        arr = [randint(-20000, 20000) for _ in range(arr_size)]
+        arr = sorted([randint(-1 * rand_range, rand_range)
+                      for _ in range(arr_size)])
+        # print(arr)
         durations = []
         for _ in range(count):
-            ele = randint(-20000, 20000)
+            ele = randint(-1 * rand_range, rand_range)
+            # print(ele)
             start = time.time()
             search.search(arr, ele)
             duration = (time.time() - start) * 1000
@@ -23,9 +28,10 @@ def main():
         if last != 0:
             ratio = duration / last
         last = duration
-        print("Time taken to search in array of size: {} is {:.3f} with ratio: {:.3f}".format(
+        print("Time taken to search in array of size: {} is {:.3f}ms with ratio: {:.3f}".format(
             arr_size, duration, ratio))
         arr_size *= 2
+        # break
     pass
 
 
