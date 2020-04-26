85e6b9def46cba064f3fe0e664749ef73ffa1841
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-04-26 19:27:41 +0530

    Running algo for seeing trend

--- triple_sum/brute_force.py
+++ triple_sum/brute_force.py
@@ -8,6 +8,16 @@ class TripleSum:
     def is_triple_sum(self, x, y, z):
         return x + y + z == 0
 
+    def count_triple_sums(self):
+        count = 0
+        l = len(self.arr)
+        for i in range(l):
+            for j in range(i + 1, l):
+                for k in range(j + 1, l):
+                    if self.is_triple_sum(self.arr[i], self.arr[j], self.arr[k]):
+                        count += 1
+        return count
+
     def find_triple_sums(self):
         triple_sums = []
         l = len(self.arr)
@@ -27,5 +37,14 @@ class TripleSum:
         out["arr"] = ", ".join([str(ele) for ele in self.arr])
         out["triple_sums"] = [", ".join([str(i) for i in t])
                               for t in triple_sums]
+        with open(file_name, "w") as f:
+            json.dump(out, f, indent=4)
+
+    def save_triple_count(self, triple_count, duration):
+        file_name = __file__.split(".")[0] + ".json"
+        out = {}
+        out["count"] = str(triple_count)
+        out["duration"] = str(duration)
+        out["arr"] = ", ".join([str(ele) for ele in self.arr])
         with open(file_name, "w") as f:
             json.dump(out, f, indent=4)
--- triple_sum/run_triple_sum.py
+++ triple_sum/run_triple_sum.py
@@ -1,21 +1,37 @@
 import random
 import sys
 import time
+import math
 # import timeit
 
 from brute_force import TripleSum
 
 
-def gen_random_arr(bound):
+def gen_random_arr(size):
+    bound = 2000
     arr = [random.randint(-1 * bound, bound)
-           for i in range(-1 * bound, bound)]
+           for i in range(size)]
     return arr
 
 
+def run_triple_sum():
+    last = 0
+    ratio = 1
+    i = 2
+    while True:
+        arr = gen_random_arr(i)
+        ts = TripleSum(arr)
+        start_time = time.time()
+        count = ts.count_triple_sums()
+        duration = time.time() - start_time
+        if last != 0:
+            ratio = duration / last
+        last = duration
+        # ts.save_triple_count(count, duration)
+        print("For \n\tsize:\t\t{}\n\tcount:\t\t{}\n\tduration:\t{:.3f}\n\tlog_ratio:\t{}".format(
+            i, count, duration, math.log2(ratio)))
+        i *= 2
+
+
 if __name__ == "__main__":
-    arr = gen_random_arr(int(sys.argv[1]))
-    ts = TripleSum(arr)
-    start_time = time.time()
-    triple_sums = ts.find_triple_sums()
-    duration = time.time() - start_time
-    ts.save_triple_sums(triple_sums, duration)
+    run_triple_sum()
