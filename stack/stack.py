9377b3965f3d0c328956a0d78b5ea64fa7e28092
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-04-28 14:21:07 +0530

    adding len, size and isempty methods

--- stack/stack.py
+++ stack/stack.py
@@ -12,13 +12,25 @@ class Stack:
             del self._arr[-1]
         return ele
 
+    def is_empty(self):
+        return len(self._arr) == 0
+
+    def size(self):
+        return len(self)
+
+    def __len__(self):
+        return len(self._arr)
+
 
 if __name__ == "__main__":
     s = Stack()
     s.push(20)
     s.push("hello")
     s.push("2")
+    print(s.size())
+    print(len(s))
     print(s.pop())
     print(s.pop())
     print(s.pop())
+    print(s.is_empty())
     print(s.pop())
