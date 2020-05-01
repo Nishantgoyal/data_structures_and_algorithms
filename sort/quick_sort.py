79b7af90e75b787a7d7bdebd9edcedbcb92f15b7
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-05-01 11:00:07 +0530

    quick sort implementation - minor fix

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -6,22 +6,36 @@ class Sort:
         self.sub_sort(0, len(self.arr) - 1)
 
     def swap(self, i, j):
+        # print("Swapping ele A[i]: {} at i: {} and ele A[j] {} at j: {}".format(
+        #     self.arr[i], i, self.arr[j], j))
         self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
+        # print("Array after swap: {}".format(self.arr))
 
     def partition(self, left, right):
         i = left
         j = right + 1
         while True:
+            # print(self.arr)
+            # print(i, j)
+            # print(left, right)
+            # print(self.arr[left])
+            # print("true")
+            # if i >= j:
+            #     break
             i += 1
-            while self.arr[i] < self.arr[left]:
-                i += 1
-                if i == right:
-                    break
+            if i < right:
+                while self.arr[i] < self.arr[left]:
+                    # print("i:{}".format(i))
+                    i += 1
+                    if i == right:
+                        break
             j -= 1
-            while self.arr[j] > self.arr[left]:
-                j -= 1
-                if j == left:
-                    break
+            if j > left:
+                while self.arr[j] > self.arr[left]:
+                    # print("j:{}".format(j))
+                    j -= 1
+                    if j == left:
+                        break
             if i >= j:
                 break
             self.swap(i, j)
@@ -29,8 +43,11 @@ class Sort:
         return j
 
     def sub_sort(self, left, right):
+        # print("subsort {} {}".format(left, right))
+        # print(self.arr)
         if left >= right:
             return
         j = self.partition(left, right)
         self.sub_sort(left, j - 1)
         self.sub_sort(j + 1, right)
+        # print(self.arr)
--- sort/run_sorting.py
+++ sort/run_sorting.py
@@ -12,7 +12,7 @@ from quick_sort import Sort
 
 
 def main():
-    bound = 30
+    bound = 300000
     arr_size = 1
     last_duration = 0
     ratio = 1
@@ -22,11 +22,11 @@ def main():
         # arr = list(set(arr))
         s = Sort(arr)
 
-        print(s.arr)
+        # print(s.arr)
         start = time.time()
         s.sort()
         duration_in_sec = time.time() - start
-        print(s.arr)
+        # print(s.arr)
 
         if last_duration != 0:
             ratio = duration_in_sec / last_duration
@@ -38,7 +38,7 @@ def main():
         print("Duration: {:3f}".format(duration_in_sec))
         print("Ratio: {:.3f}".format(math.log2(ratio)))
         arr_size *= 2
-        time.sleep(5)
+        # time.sleep(5)
 
 
 if __name__ == "__main__":
