91f1b6f60a369430601b1385c24ab114d1faa685
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-05-13 12:37:59 +0530

    quick sort with 3 way partitioning done

--- sort/run_sorting.py
+++ sort/run_sorting.py
@@ -8,38 +8,50 @@ import math
 # from selection_sort import Sort
 # from shell_sort import Sort
 # from merge_sort import Sort
-from quick_sort import Sort
+# from quick_sort import Sort
+from quick_sort_3_way_partitioning import Sort
 
 
-def main():
-    bound = 300000
+def run_single_test():
+    bound = 30
+    arr_size = 10
+    # seed(bound)
+    arr = [randint(0, bound) for _ in range(arr_size)]
+    print("Initialised Array: {}..!!".format(arr))
+    s = Sort(arr)
+    s.sort()
+    print(s.arr)
+
+
+def run_tests():
+    bound = 3000000
     arr_size = 1
     last_duration = 0
-    ratio = 1
-    # seed(bound)
+    ratio = 1.0
     while True:
         print("{}".format("<") * 10, "{}".format("==")
               * 10, "{}".format(">") * 10)
         arr = [randint(-1 * bound, bound) for _ in range(arr_size)]
         print("Initialised Array of size: {}..!!".format(arr_size))
-        # arr = list(set(arr))
         s = Sort(arr)
 
-        # print(s.arr)
         start = time.time()
         s.sort()
         duration_in_sec = time.time() - start
-        # print(s.arr)
 
         if last_duration != 0:
             ratio = duration_in_sec / last_duration
         last_duration = duration_in_sec
 
-        # print("Array Size: {}".format(len(arr)))
         print("Duration: {:3f}".format(duration_in_sec))
         print("Ratio: {:.3f}".format(math.log2(ratio)))
         arr_size *= 2
         # time.sleep(5)
+
+
+def main():
+    # run_tests()
+    run_single_test()
 
 
 if __name__ == "__main__":
