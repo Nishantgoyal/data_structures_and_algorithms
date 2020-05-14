9eb4f11a0999b23a858614753b08463c3b2c31ca
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-05-14 12:29:32 +0530

    binary heap - get element

--- binary_heap/binary_heap.py
+++ binary_heap/binary_heap.py
@@ -43,21 +43,63 @@ class BinaryHeap:
             node_count += 1
 
         for heap_level in heap_print:
-            heap_string += "\n"
             for ele in heap_level:
                 heap_string += "*" * (2 ** node_level + 1)
                 heap_string += str(ele)
-                heap_string += "*" * (2 ** node_level + 1)
+                # heap_string += "*" * (2 ** node_level + 1)
             node_level -= 1
+            heap_string += "\n"
         print(heap_string)
 
+    def sink(self, index_ele):
+        print("Sinking {}".format(index_ele))
+
+        index_fc = 2 * (index_ele + 1) - 1
+        index_sc = 2 * (index_ele + 1)
+        max_index = index_ele
+        max_value = self._heap[index_ele]
+
+        if index_fc < self._heap_len:
+            if self._type == "Max":
+                if self._heap[index_fc] > max_value:
+                    max_index = index_fc
+                    max_value = self._heap[index_fc]
+            if self._type == "Min":
+                if self._heap[index_fc] < max_value:
+                    max_index = index_fc
+                    max_value = self._heap[index_fc]
+        if index_sc < self._heap_len:
+            if self._type == "Max":
+                if self._heap[index_sc] > max_value:
+                    max_value = self._heap[index_sc]
+                    max_index = index_sc
+            if self._type == "Min":
+                if self._heap[index_sc] < max_value:
+                    max_value = self._heap[index_sc]
+                    max_index = index_sc
+
+        if index_ele != max_index:
+            self._heap[index_ele], self._heap[max_index] = self._heap[max_index], self._heap[index_ele]
+            self.sink(max_index)
+
     def get_ele(self):
-        pass
+        if self._heap_len <= 0:
+            print("Empty Heap...")
+            return
+        ele = self._heap[0]
+        self._heap_len -= 1
+        self._heap[0] = self._heap[self._heap_len]
+        self.sink(0)
+        self._heap.pop(self._heap_len)
+        return ele
 
 
 if __name__ == "__main__":
-    arr = [54, 65, 32, 89, 56, 3, 84, 24, 57, 25, 86, 64, 27]
+    arr = [54, 65, 32, 89, 56, 3, 84, 24, 57, 25, 86, 64, 27, 3]
     # print(arr)
-    heap = BinaryHeap()
+    heap = BinaryHeap("Min")
     heap.create_heap(arr)
+    heap.print_heap()
+
+    heap.get_ele()
     heap.print_heap()
