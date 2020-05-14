f2bf2003289d916a9a594cfbff7a883afca6821a
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-05-14 17:40:02 +0530

    implemented heap sort without recursion

--- sort/heap_sort.py
+++ sort/heap_sort.py
@@ -6,7 +6,7 @@ class Sort:
     class BinaryHeap:
         def __init__(self):
             self._heap = []
-            self._heap_len = len(self._heap)
+            self._heap_len = 0
 
         def create_heap(self, arr):
             # print("Creating heap with Arr:{}".format(arr))
@@ -20,34 +20,41 @@ class Sort:
             self._heap_len += 1
 
         def swim(self, index_ele):
-            # print("\t\tSwimming {} at {}".format(
-            #     self._heap[index_ele], index_ele))
+            # print("\t\tSwim Function. Index:{}".format(index_ele))
             index_par = (index_ele - 1) // 2
-            if self._heap[index_ele] < self._heap[index_par]:
-                self._heap[index_ele], self._heap[index_par] = self._heap[index_par], self._heap[index_ele]
-                if index_par > 0:
-                    self.swim(index_par)
+            while index_par >= 0:
+                # print("\t\tSwimming {} at {}".format(
+                #     self._heap[index_ele], index_ele))
+                if self._heap[index_ele] < self._heap[index_par]:
+                    # print("\t\tExchanging {} with {}".format(
+                    #     index_ele, index_par))
+                    self._heap[index_ele], self._heap[index_par] = self._heap[index_par], self._heap[index_ele]
+                index_ele = index_par
+                index_par = (index_ele - 1) // 2
 
         def sink(self, index_ele):
-            # print("Sinking {}".format(index_ele))
+            # print("Sink Function")
+            while True:
+                # print("Sinking {}".format(index_ele))
+                index_fc = 2 * (index_ele + 1) - 1
+                index_sc = 2 * (index_ele + 1)
+                max_index = index_ele
+                max_value = self._heap[index_ele]
 
-            index_fc = 2 * (index_ele + 1) - 1
-            index_sc = 2 * (index_ele + 1)
-            max_index = index_ele
-            max_value = self._heap[index_ele]
+                if index_fc < self._heap_len:
+                    if self._heap[index_fc] < max_value:
+                        max_index = index_fc
+                        max_value = self._heap[index_fc]
+                if index_sc < self._heap_len:
+                    if self._heap[index_sc] < max_value:
+                        max_value = self._heap[index_sc]
+                        max_index = index_sc
 
-            if index_fc < self._heap_len:
-                if self._heap[index_fc] < max_value:
-                    max_index = index_fc
-                    max_value = self._heap[index_fc]
-            if index_sc < self._heap_len:
-                if self._heap[index_sc] < max_value:
-                    max_value = self._heap[index_sc]
-                    max_index = index_sc
+                if index_ele == max_index:
+                    break
 
-            if index_ele != max_index:
                 self._heap[index_ele], self._heap[max_index] = self._heap[max_index], self._heap[index_ele]
-                self.sink(max_index)
+                index_ele = max_index
 
         def get_ele(self):
             if self._heap_len <= 0:
