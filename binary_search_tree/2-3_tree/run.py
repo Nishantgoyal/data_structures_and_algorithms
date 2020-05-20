de7c4dba01a3e89a8068140eef89870a68a3d089
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-05-20 10:50:23 +0530

    added run class

---
+++ binary_search_tree/2-3_tree/run.py
@@ -0,0 +1,45 @@
+from two_three_tree import TwoThreeTree
+
+
+def main():
+    pass
+
+    tree = TwoThreeTree()
+
+    # print()
+    # tree.insert(14)
+
+    # print()
+    # tree.insert(2)
+
+    # print()
+    # tree.insert(8)
+
+    # print()
+    # tree.insert(6)
+
+    # print()
+    # tree.insert(26)
+
+    # print()
+    # tree.insert(5)
+
+    # print("\nTree: \n{}".format(tree.root))
+    # # tree.dump_tree()
+    # # seed(20)
+    # # ele_count = 1
+    # # while ele_count > 0:
+    # #     ele_count -= 1
+    # #     ele = randint(0, 100)
+    # #     val = chr(ord("a") + randint(0, 25))
+    # #     tree.insert(ele, val)
+
+    # # # tree.get(22)
+    # # # tree.get(9)
+    # # # # print(val)
+
+    # # tree.delete(92)
+
+
+if __name__ == "__main__":
+    main()
