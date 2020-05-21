5b637faa826df63d274ac03e5601d3353644e753
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-05-21 09:42:36 +0530

    added classes for Nodes

---
+++ binary_search_tree/two_three_tree/node.py
@@ -0,0 +1,34 @@
+class OneNode:
+    '''
+        APIs
+    '''
+
+    def __init__(self):
+        pass
+
+
+class TwoNode:
+    '''
+        APIs
+    '''
+
+    def __init__(self):
+        pass
+
+
+class ThreeNode:
+    '''
+        APIs
+    '''
+
+    def __init__(self):
+        pass
+
+
+class FourNode:
+    '''
+        APIs
+    '''
+
+    def __init__(self):
+        pass
