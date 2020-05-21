d9a22b46dc2175bfe73ff4cfc06938a2c351aa7c
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-05-21 10:09:52 +0530

    implemented method get children

--- binary_search_tree/two_three_tree/node.py
+++ binary_search_tree/two_three_tree/node.py
@@ -4,6 +4,8 @@ class TwoNode:
     '''
         APIs
          - repr
+         - is_leaf?
+         - get_children
     '''
 
     def __init__(self, key, parent=None):
@@ -15,10 +17,24 @@ class TwoNode:
     def __repr__(self):
         return "({})".format(self.key)
 
+    def is_leaf(self):
+        pass
+
+    def get_children(self):
+        children = []
+        if self.tree_left:
+            children.append(self.tree_left)
+        if self.tree_right:
+            children.append(self.tree_right)
+        return children
+
 
 class ThreeNode:
     '''
         APIs
+         - repr
+         - is_leaf?
+         - get_children
     '''
 
     def __init__(self, l_key, r_key, parent):
@@ -31,3 +47,16 @@ class ThreeNode:
 
     def __repr__(self):
         return "({},{})".format(self.l_key, self.r_key)
+
+    def is_leaf(self):
+        pass
+
+    def get_children(self):
+        children = []
+        if self.tree_left:
+            children.append(self.tree_left)
+        if self.tree_mid:
+            children.append(self.tree_mid)
+        if self.tree_right:
+            children.append(self.tree_right)
+        return children
