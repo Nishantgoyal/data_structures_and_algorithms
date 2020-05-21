25127498b8de8a16958424fbde23e9d89adf1a47
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-05-21 10:06:19 +0530

    added repr for nodes

--- binary_search_tree/two_three_tree/node.py
+++ binary_search_tree/two_three_tree/node.py
@@ -1,19 +1,19 @@
-class OneNode:
-    '''
-        APIs
-    '''
-
-    def __init__(self):
-        pass
 
 
 class TwoNode:
     '''
         APIs
+         - repr
     '''
 
-    def __init__(self):
-        pass
+    def __init__(self, key, parent=None):
+        self.key = key
+        self.parent = parent
+        self.tree_left = None
+        self.tree_right = None
+
+    def __repr__(self):
+        return "({})".format(self.key)
 
 
 class ThreeNode:
@@ -21,14 +21,13 @@ class ThreeNode:
         APIs
     '''
 
-    def __init__(self):
-        pass
+    def __init__(self, l_key, r_key, parent):
+        self.l_key = l_key
+        self.r_key = r_key
+        self.parent = parent
+        self.tree_left = None
+        self.tree_mid = None
+        self.tree_right = None
 
-
-class FourNode:
-    '''
-        APIs
-    '''
-
-    def __init__(self):
-        pass
+    def __repr__(self):
+        return "({},{})".format(self.l_key, self.r_key)
