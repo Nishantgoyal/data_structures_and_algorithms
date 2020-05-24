dfa100a208a5444a3d6458a171b9960fea98ac36
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-05-24 19:03:59 +0530

    testing program

--- binary_search_tree/two_three_tree/run.py
+++ binary_search_tree/two_three_tree/run.py
@@ -2,7 +2,18 @@ from two_three_tree import TwoThreeTree
 
 
 def main():
-    pass
+    tree = TwoThreeTree()
+    while True:
+        print(
+            "Please choose an operation to perform: {I - Insert}. Any other key to Exit...")
+        inp = input()
+        if inp == "I":
+            print("Please input a Integer to insert..")
+            key = int(input())
+            tree.insert(key)
+            tree.print_tree()
+        else:
+            break
 
 
 if __name__ == "__main__":
--- binary_search_tree/two_three_tree/two_three_tree.py
+++ binary_search_tree/two_three_tree/two_three_tree.py
@@ -1,8 +1,24 @@
+from node import TwoNode, ThreeNode
+
+
 class TwoThreeTree:
     '''
         APIs:
-         - Tree Repr
+            - __init__
+            - print_tree
     '''
 
     def __init__(self):
         self.root = None
+
+    def print_tree(self):
+        if self.root is None:
+            print("[]")
+        else:
+            self.root.print_tree()
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = TwoNode(key)
+        else:
+            self.root.insert_key(key)
