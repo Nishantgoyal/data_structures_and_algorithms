badfb78f51235ea3e4ce8c5e8613e59d493c0730
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-06-05 09:13:50 +0530

    GCD for array

--- GCD/run.py
+++ GCD/run.py
@@ -1,9 +1,18 @@
+import sys
 from gcd import GCD
 
 
+def calculate_GCD_of_Arr(arr):
+    gcd = arr[0]
+    for ele in arr[1:]:
+        gcd = GCD(gcd, ele).gcd
+    return gcd
+
+
 def main():
-    gcd = GCD(1230, 600)
-    print(gcd.gcd)
+    num_array = list(map(int, sys.argv[1].split(",")))
+    gcd = calculate_GCD_of_Arr(num_array)
+    print(gcd)
 
 
 if __name__ == "__main__":
