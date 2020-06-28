8eb68c32ac6cb7f57a51255ffd5ce046318aca2a
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-06-28 20:29:47 +0530

    dynamic change

--- coin_change/dynamic_change.py
+++ coin_change/dynamic_change.py
@@ -1,24 +1,47 @@
+import copy
+
+
 class Change:
     def __init__(self, denominations):
         self.denominations = sorted(denominations, reverse=True)
         self._cache = {}
 
-    def get_change(self, amount):
-        # print("Getting Change for Amount: {}".format(amount))
+    def change(self, amount):
         if amount in self._cache:
-            return self._cache[amount]
+            return self._cache[amount]["change"]
+
         if amount == 0:
+            self._cache[amount] = {}
+            self._cache[amount]["change"] = 0
+            self._cache[amount]["curr"] = {}
             return 0
-        # min(get_change(amount - den) for all den)
+
         min_change = None
-        choices = []
+        curr = {}
         for d in self.denominations:
             if d <= amount:
-                change = 1 + self.get_change(amount - d)
+                change = 1 + self.change(amount - d)
                 if min_change is None:
                     min_change = change
+                    curr = copy.deepcopy(self._cache[amount - d]["curr"])
+                    if d not in curr:
+                        curr[d] = 0
+                    curr[d] += 1
                 else:
                     if min_change > change:
                         min_change = change
-        self._cache[amount] = min_change
+                        curr = copy.deepcopy(self._cache[amount - d]["curr"])
+                        if d not in curr:
+                            curr[d] = 0
+                        curr[d] += 1
+        self._cache[amount] = {}
+        self._cache[amount]["change"] = min_change
+        self._cache[amount]["curr"] = copy.deepcopy(curr)
         return min_change
+
+    def get_change(self, amount):
+        # min_change = None
+        for i in range(1, amount + 1):
+            # print("Getting Change for: {}".format(i))
+            self.change(i)
+        return self._cache[amount]
--- coin_change/main.py
+++ coin_change/main.py
@@ -7,18 +7,20 @@ from dynamic_change import Change
 
 
 def main():
-    denominations = [1, 2, 5, 10]
+    # denominations = [1, 8, 20]
+    denominations = [1, 2, 5, 7, 10, 15]
     change_class = Change(denominations)
 
     while True:
         amount = randint(1, 100)
-        print(amount)
+        # amount = 32
+        # print(amount)
         # amount = 10
         change = change_class.get_change(amount)
         # change_class.get_change(amount)
         print("Amount: {}\tChange: {}".format(amount, change))
-        # time.sleep(1)
-        break
+        time.sleep(1)
+        # break
 
 
 if __name__ == "__main__":
